19b5dc6da1fd86b3e3c2944b8d0be44972d3f0ca
sauravMir <saurav_41@yahoo.com>
2022-11-17 00:31:59 +0900

    wifi handled
    - notification bar show
    - wifi button click on samsung
    - close notification bar
    - back button click

--- src/test/java/ioSampleTest.java
+++ src/test/java/ioSampleTest.java
@@ -1,11 +1,19 @@
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.connection.ConnectionState;
+import io.appium.java_client.android.connection.ConnectionStateBuilder;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -61,14 +69,49 @@ public class ioSampleTest {
     public void click1stButton1() throws InterruptedException {
         System.out.println("click1stButton1");
         System.out.println("Thread: "  + ": "+ Thread.currentThread().getName());
-        wait.until(ExpectedConditions.visibilityOfElementLocated(firstButtonBy)).click();
+        driver.setConnection(new ConnectionStateBuilder().withWiFiDisabled().withDataDisabled().build());
+       // driver.runAppInBackground(Duration.ofMillis(3000));
+        driver.getConnection();
+        driver.openNotifications();
+        Thread.sleep(2000);
+        new TouchAction(driver).tap(PointOption.point(new Point(117, 375))).perform();
+        new TouchAction(driver).tap(PointOption.point(new Point(80, 80)));
+        new TouchAction(driver).tap(PointOption.point(new Point(80, 80)));
+        new TouchAction(driver).tap(PointOption.point(new Point(280, 280)));
+        new TouchAction(driver).tap(PointOption.point(new Point(380, 380)));
+        new TouchAction(driver).tap(PointOption.point(new Point(80, 80)));
+        new TouchAction(driver).tap(PointOption.point(new Point(180, 180)));
+        ((AndroidDriver) driver).pressKey(new KeyEvent().withKey(AndroidKey.BACK));
+
+        //driver.closeApp();
+        driver.toggleWifi();
+        pressWifi();
+       /* wait.until(ExpectedConditions.visibilityOfElementLocated(firstButtonBy)).click();
         Thread.sleep(3000);
         contextSwitch(2);
         Thread.sleep(2000);
         System.out.println("Test1: " + driver.getCurrentUrl());
         contextSwitch(1);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(crossButtonBy)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(crossButtonBy)).click();*/
+
         Thread.sleep(2000);
+        connectWifi();
+  }
+
+    public void connectWifi() {
+        driver.setConnection(new ConnectionStateBuilder().withWiFiEnabled().withDataEnabled().build());
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void pressWifi(){
+
+            try {
+                ((AndroidDriver)driver).toggleWifi();
+                Thread.sleep(2000);
+            } catch (Exception e ) {
+                System.out.println("Error turning off flight mode.");
+            }
+
     }
 
 
